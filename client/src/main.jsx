5fa3344035b727059ab076f6451637de45aa2cfb
Suhani1234-5 <suhani.garg.s64@kalvium.community>
2025-04-24 11:11:10 +0530

    ggle

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+    <GoogleOAuthProvider >
       <App />
     </GoogleOAuthProvider>
   </React.StrictMode>
