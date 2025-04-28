9a36f3bfd3e9bafc17e5f6058736a9c13ab7cde1
Suhani1234-5 <suhani.garg.s64@kalvium.community>
2025-04-29 01:25:18 +0530

    web

--- backend/routes/notifyRoutes.js
+++ backend/routes/notifyRoutes.js
@@ -1,26 +1,26 @@
-// const express = require('express');
-// const {
-//   sendReminder,
-//   getAllNotifications,
-//   sendBloodRequest,
-//   respondToBloodRequest,
-//   getEmergencyBloodRequests
-// } = require('../controller/notifyController');
-// const router = express.Router();
+const express = require('express');
+const {
+  sendReminder,
+  getAllNotifications,
+  sendBloodRequest,
+  respondToBloodRequest,
+  getEmergencyBloodRequests
+} = require('../controller/notifyController');
+const router = express.Router();
 
-// // Route to send a new general notification
-// router.post('/send', sendReminder);
+// Route to send a new general notification
+router.post('/send', sendReminder);
 
-// // Route to send emergency blood request notification
-// router.post('/blood-request', sendBloodRequest);
+// Route to send emergency blood request notification
+router.post('/blood-request', sendBloodRequest);
 
-// // Route to respond to a blood request
-// router.post('/respond', respondToBloodRequest);
+// Route to respond to a blood request
+router.post('/respond', respondToBloodRequest);
 
-// // Route to get all active notifications
-// router.get('/all', getAllNotifications);
+// Route to get all active notifications
+router.get('/all', getAllNotifications);
 
-// // Route to get only emergency blood requests
-// router.get('/emergency', getEmergencyBloodRequests);
+// Route to get only emergency blood requests
+router.get('/emergency', getEmergencyBloodRequests);
 
-// module.exports = router;
+module.exports = router;
