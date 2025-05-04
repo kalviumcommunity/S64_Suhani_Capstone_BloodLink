e209291db20f0ccaee4bce70d1a01605fa917da2
Suhani1234-5 <suhani.garg.s64@kalvium.community>
2025-05-05 00:03:56 +0530

    done

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,7 @@ const server = http.createServer(app);
 // Middleware
 app.use(express.json());
 app.use(cors({
-  origin: 'https://bloodlink-app.netlify.app/'  || 'http://localhost:5173',
+  origin: 'https://bloodlink-app.netlify.app'  || 'http://localhost:5173',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
