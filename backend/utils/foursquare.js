9e6d703574c414b7b7b636cc7e05c374342e61c9
Suhani1234-5 <suhanigarg59@gmail.com>
2026-08-24 12:43:45 +0530

    done

--- backend/utils/foursquare.js
+++ backend/utils/foursquare.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 require('dotenv').config();
 
 const foursquare = axios.create({
-  baseURL: 'https://api.foursquare.com/v3/places',
+  baseURL: 'https://api.foursquare.com/places',
   headers: {
     Accept: 'application/json',
     Authorization: process.env.FSQ_API_KEY,
